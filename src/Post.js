e69c9d40383a2c7af1135004c250641abef5d4ce
matheussouzalima <matholaslima@gmail.com>
2022-09-18 16:08:20 -0300

    Criação de itens dinamicos

--- src/Post.js
+++ src/Post.js
@@ -1,4 +1,11 @@
+import { useState } from "react"
+
 export default function Post(props) {
+    let [likes,setLikes] = useState(props.likes);
+    let [heart,setHeart] = useState('heart-outline');
+    let [classHeart,setClassHeart] = useState('');
+    let [save,setSave] = useState("bookmark-outline");
+
     return(
         <div class="post">
             <div class="topo">
@@ -18,22 +25,58 @@ export default function Post(props) {
             <div class="fundo">
                 <div class="acoes">
                 <div>
-                    <ion-icon name="heart-outline"></ion-icon>
+                    <ion-icon class={classHeart} onClick={()=>{curtir(likes,setLikes,heart,setHeart,setClassHeart)}} name={heart}></ion-icon>
                     <ion-icon name="chatbubble-outline"></ion-icon>
                     <ion-icon name="paper-plane-outline"></ion-icon>
                 </div>
                 <div>
-                    <ion-icon name="bookmark-outline"></ion-icon>
+                    <ion-icon onClick={()=>{salvar(save,setSave)}} name={save}></ion-icon>
                 </div>
                 </div>
 
                 <div class="curtidas">
                     <img src={props.imgLike} alt={props.like}/>
                     <div class="texto">
-                        Curtido por <strong>{props.like}</strong> e <strong>outras {props.likes} pessoas</strong>
+                        Curtido por <strong>{props.like}</strong> e <strong>outras {likes} pessoas</strong>
                     </div>
                 </div>
             </div>
         </div>
     )
+}
+
+function curtir(likes,setLikes,heart,setHeart,setClassHeart){
+    let num = parseInt(likes);
+    let str = likes.toString();
+    let num2 = str.split('.');
+    let mil;
+
+    num*=1000;
+    num+=parseInt(num2[1]);
+
+    if(heart === 'heart-outline'){
+        setHeart('heart');
+        setClassHeart('red');
+        num+=1;
+        mil = parseInt(num/1000);
+        str = mil + '.' + num%1000;
+        setLikes(str);
+    }
+    else{
+        setHeart('heart-outline');
+        setClassHeart('')
+        num-=1;
+        mil = parseInt(num/1000);
+        str = mil + '.' + num%1000;
+        setLikes(str);
+    }
+}
+
+function salvar(save,setSave){
+    if(save==='bookmark-outline'){
+        setSave('bookmark');
+    }
+    else{
+        setSave('bookmark-outline');
+    }
 }
--- src/Usuario.js
+++ src/Usuario.js
@@ -1,14 +1,32 @@
+import { useState } from "react"
+
 export default function Usuario(props) {
+    let [name,setName] = useState(props.name);
+    let [img,setImg] = useState(props.img);
     return (
         <div class="usuario">
-          <img src={props.img} alt={props.name}/>
+          <img onClick={()=>{changeImg(setImg)}} src={img} alt={name}/>
           <div class="texto">
             <strong>{props.user}</strong>
             <span>
-              {props.name}
-              <ion-icon name="pencil"></ion-icon>
+              {name}
+              <ion-icon onClick={()=>{changeName(setName)}} name="pencil"></ion-icon>
             </span>
           </div>
         </div>
     )
+}
+
+function changeName(setName){
+  let name = prompt('Escolha a nova imagem de perfil:');
+  if(name!==undefined){
+    setName(name);
+  }
+}
+
+function changeImg(setImg){
+  let img = prompt('Escolha o novo nome:');
+  if(img!==undefined){
+    setImg(img);
+  }
 }
